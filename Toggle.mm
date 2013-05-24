9d16a26dd0f43c90cbfcdb0269e556bfd891d316
Jason Recillo <jaysan1292@gmail.com>
2013-05-24 11:54:11 -0400

    Read user preferences instead of calling system commands

--- Toggle.mm
+++ Toggle.mm
@@ -1,27 +1,21 @@
 #import <Foundation/Foundation.h>
 #import <notify.h>
 
+#define kFluxPrefsLocation @"/var/mobile/Library/Preferences/org.herf.flux.plist"
+#define kEnableKey @"enable"
+
 extern "C" BOOL isEnabled() {
-	FILE* ps;
-	char output[256];
-
-	ps = popen("ps ax|grep flux", "r");
-
-	while (fgets(output, 256, ps) != NULL) {
-		NSString* out = [NSString stringWithCString:output encoding:NSASCIIStringEncoding];
-		NSRange running = [out rangeOfString:@"/usr/libexec/flux"];
-
-		if(running.location != NSNotFound) return YES;
-	}
-	return NO;
+	NSMutableDictionary* pref = [NSMutableDictionary dictionaryWithContentsOfFile:kFluxPrefsLocation];
+	return [[pref objectForKey:kEnableKey] boolValue];
 }
 
 extern "C" void setState(BOOL enabled) {
-	if(enabled) {
-		notify_post("com.jaysan1292.fluxon");
-	} else {
-		notify_post("com.jaysan1292.fluxoff");
-	}
+	NSMutableDictionary* pref = [NSMutableDictionary dictionaryWithContentsOfFile:kFluxPrefsLocation];
+
+	[pref setValue:[NSNumber numberWithBool:enabled] forKey:kEnableKey];
+	[pref writeToFile:kFluxPrefsLocation atomically:YES];
+
+	notify_post("org.herf.flux-enable");
 }
 
 extern "C" BOOL isCapable() { return YES; }
